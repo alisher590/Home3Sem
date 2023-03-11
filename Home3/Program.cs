ddba5dfff865dbbca21eebbd8ef5197a3498d55f
алишер <aliser@MacBook-Air-aliser.local>
2023-03-11 00:54:12 -0800

    Add  Home 3

---
+++ Home3/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+try
+{
+    Console.Write("Введите число N: ");
+    int num = int.Parse(Console.ReadLine() ?? "");
+    for (int n = 1; n <= num; n++)
+    {
+        int i = n * n * n;
+        Console.WriteLine($"Таблица кубов:{i}");
+    }
+}
+catch (Exception exc)
+{
+    Console.WriteLine($"Ошибка буквы и 'вещественые числа' не принимаем: {exc.Message}");
+}
+
